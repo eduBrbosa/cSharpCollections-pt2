b203b281d4c0e8049b9d31ee4b09bb0258f7dedc
eduBrbosa <d.dbarbosa142@gmail.com>
2025-11-02 19:31:50 -0300

    Sorted Set

---
+++ SortedSet/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISet<string> alunos = new SortedSet<string>(new ComparadorMinusculo())
+            {
+                "Vanessa Tonini",
+                "Ana Losnak",
+                "Rafael Nercessian",
+                "Priscila Stuani"
+            };
+
+
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+            alunos.Add("Fabio Gushiken");
+            alunos.Add("FABIO GUSHIKEN");
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+            Console.WriteLine();
+
+
+            ISet<string> outroConjunto = new HashSet<string>();
+
+            alunos.IsSubsetOf(outroConjunto);
+            alunos.IsSupersetOf(outroConjunto);
+            alunos.SetEquals(outroConjunto);
+            alunos.ExceptWith(outroConjunto);
+            alunos.IntersectWith(outroConjunto);
+            alunos.SymmetricExceptWith(outroConjunto);
+            alunos.UnionWith(outroConjunto);
+
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class ComparadorMinusculo : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
