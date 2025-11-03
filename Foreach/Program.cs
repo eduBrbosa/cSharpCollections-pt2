9e6b1d886ff91f7bb5603a231f4637a597b0908b
eduBrbosa <d.dbarbosa142@gmail.com>
2025-11-02 22:10:11 -0300

    Foreach

---
+++ Foreach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meses = new List<string>
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+
+            foreach (var mes in meses)
+            {
+                Console.WriteLine(mes.ToUpper());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
